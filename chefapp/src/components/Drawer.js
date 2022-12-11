e3ead86d7d053d3459d06098107df5378ed31fb7
Mariusz Nowak <m.nowak1219@gmail.com>
2022-12-11 19:30:38 +0100

    Add Drawer.

---
+++ chefapp/src/components/Drawer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import { connect } from 'react-redux'
+import { openDrawerActionCreator, closeDrawerActionCreator } from '../state/drawer'
+
+import { SwipeableDrawer, List, ListItemText, ListItem } from '@mui/material'
+
+const links = [
+  { title: 'Dodaj przepis', route: '/add-recipe' },
+  { title: 'Przepisy', route: '/recipes' },
+  { title: 'Twoje przepisy', route: 'your-recipes' },
+]
+
+const Drawer = props => {
+  return (
+    <SwipeableDrawer
+      open={props._isOpen}
+      onClose={props._close}
+      onOpen={props._open}
+    >
+      <List>
+        {links.map(link => (
+          <ListItem
+            onClick={() => {
+              props._close()
+              props.history.push(link.route)
+            }}
+            button={true}
+            key={link.title}
+          >
+            <ListItemText
+              primary={link.title}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </SwipeableDrawer>
+  )
+}
+
+const mapStateToProps = state => ({
+  _isOpen: state.drawer.isOpen
+})
+
+const mapDispatchToProps = dispatch => ({
+  _open: () => dispatch(openDrawerActionCreator()),
+  _close: () => dispatch(closeDrawerActionCreator())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(Drawer))
